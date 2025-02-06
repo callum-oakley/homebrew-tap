d7ebb5b8625100724c9c8e3a8c99c1b831a92bbc
Callum Oakley <hello@callumoakley.net>
2025-02-06 21:16:52 +0000

    jfn 2.1.1

--- jfn.rb
+++ jfn.rb
@@ -1,14 +1,14 @@
 class Jfn < Formula
-  version "2.1.0"
+  version "2.1.1"
   desc "A tool for evaluating a JavaScript function and printing the result."
   homepage "https://github.com/callum-oakley/jfn"
   
   if OS.mac?
     url "#{homepage}/releases/download/v#{version}/jfn-#{version}-macos.tar.gz"
-    sha256 "07c54aaf863dae6039bc61f843d9f0dd0466c3b00ab804142d9126ac1de98fc8"
+    sha256 "f9cedca90dfeb5b10132edb38be5ff344c50f4dd87d7b32e79675daeca0dbcaa"
   elsif OS.linux?
     url "#{homepage}/releases/download/v#{version}/jfn-#{version}-linux.tar.gz"
-    sha256 "995395fc99c32054ca77acba3a5bd6b8fc194d138430d82979b841c296e79918"
+    sha256 "6b811453ae7c7955f9f0fac2a9f84ec0272fb461ed34568d1c270cdd7fd8fa37"
   end
   
   def install
