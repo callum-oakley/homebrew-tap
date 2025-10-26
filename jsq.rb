3ab95d73b2ee17ad6962c2940dc0cb49d7984483
Callum Oakley <hello@callumoakley.net>
2025-10-26 08:08:24 +0000

    jsq 4.2.0

--- jsq.rb
+++ jsq.rb
@@ -1,15 +1,15 @@
 class Jsq < Formula
-  version "4.1.2"
+  version "4.2.0"
   desc "A tool for evaluating a JavaScript function and printing the result."
   homepage "https://github.com/callum-oakley/jsq"
   depends_on "deno"
   
   if OS.mac?
     url "#{homepage}/releases/download/v#{version}/jsq-#{version}-macos.tar.gz"
-    sha256 "04b6da9f3b40ff3934111acd816eee5cc5c1219155d2ff7425d2176aa3128315"
+    sha256 "aa830c743abb384d14d7025f9da26cdeac5b35c08bfa8f05adab3d105c17b5a4"
   elsif OS.linux?
     url "#{homepage}/releases/download/v#{version}/jsq-#{version}-linux.tar.gz"
-    sha256 "c9d242ac8c63de920311842e23f5ff30a594bc7425e148d743baef2c179c7282"
+    sha256 "2197d62f66abdd410b8b5e913a15273a5b9e1e05e413c22e93a9b7902fa193b1"
   end
   
   def install
