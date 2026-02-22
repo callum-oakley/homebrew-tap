81469f58ee55cca277ee543cf54b3ba05bc920b9
Callum Oakley <hello@callumoakley.net>
2026-02-22 00:59:22 +0000

    jog 0.2.3

--- jog.rb
+++ jog.rb
@@ -1,14 +1,14 @@
 class Jog < Formula
-  version "0.2.2"
+  version "0.2.3"
   desc "A task runner with no string substitution."
   homepage "https://github.com/callum-oakley/jog"
   
   if OS.mac?
     url "#{homepage}/releases/download/v#{version}/jog-#{version}-macos.tar.gz"
-    sha256 "a767cc1b43930d74fa407ac5a1ce49a445f2cb913abfe7510da31b9a1d5b7fb3"
+    sha256 "bffc8c111979d5b841dce6ae83d7a0c05ad28e24aefc8b40f3513e1c6eda313d"
   elsif OS.linux?
     url "#{homepage}/releases/download/v#{version}/jog-#{version}-linux.tar.gz"
-    sha256 "6b604ea37cc59700997126121cb713e6e1d928640dcf1569cf1afa5375e55f7a"
+    sha256 "e7f62f6587a3746669ba191fc851b1b4c335cef6d97fcaefba5b330efa0df8e4"
   end
   
   def install
